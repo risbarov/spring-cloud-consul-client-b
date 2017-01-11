9763d477fa675c6f81f9fc8937f01c588a9ed458
Исбаров Руслан Фахраддинович <risbarov@technoserv.com>
2017-01-12 02:42:57 +0300

    SCCD-00000 Обработка ошибки для случая, когда один из экземпляров сервиса недоступен

--- src/main/java/org/sf/cloud/consul/discovery/web/DiscoveryClientController.java
+++ src/main/java/org/sf/cloud/consul/discovery/web/DiscoveryClientController.java
@@ -2,15 +2,21 @@ package org.sf.cloud.consul.discovery.web;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DiscoveryClientController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DiscoveryClientController.class);
 
 	private static final String SERVICE_ID = "spring-cloud-consul-discovery-client-a";
 
@@ -24,13 +30,17 @@ public class DiscoveryClientController {
 		discoveryClient.getInstances(SERVICE_ID).forEach((ServiceInstance s) -> {
 			URI serviceUri = s.getUri();
 
-			String serviceResponse = new RestTemplate().getForObject(serviceUri.resolve("/ping"), String.class);
+			try {
+				String serviceResponse = new RestTemplate().getForObject(serviceUri.resolve("/ping"), String.class);
 
-			stringBuilder.append("[")
-				.append(serviceUri.toString())
-				.append(" ::: ")
-				.append(serviceResponse)
-				.append("]");
+				stringBuilder.append("[")
+					.append(serviceUri.toString())
+					.append(" ::: ")
+					.append(serviceResponse)
+					.append("]");
+			} catch (RestClientException e) {
+				LOG.warn("Cannot communicate with service {} deployed on {}", s.getServiceId(), serviceUri);
+			}
 		});
 
 		return stringBuilder.toString();
