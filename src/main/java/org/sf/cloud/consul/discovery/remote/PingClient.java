0dd5fde4c75a932fcd5112e3a233c24f384eda82
Исбаров Руслан Фахраддинович <risbarov@technoserv.com>
2017-01-12 02:44:26 +0300

    SCCD-00000 Обработка ошибки для случая, когда один из экземпляров сервиса недоступен

--- src/main/java/org/sf/cloud/consul/discovery/remote/PingClient.java
+++ src/main/java/org/sf/cloud/consul/discovery/remote/PingClient.java
@@ -1,4 +1,4 @@
-package org.sf.cloud.consul.discovery.client;
+package org.sf.cloud.consul.discovery.remote;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "spring-cloud-consul-discovery-client-a")
-public interface ServiceAClient {
+public interface PingClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/ping", consumes = MediaType.APPLICATION_JSON_VALUE)
 	String ping();
--- src/main/java/org/sf/cloud/consul/discovery/web/FeignClientController.java
+++ src/main/java/org/sf/cloud/consul/discovery/web/FeignClientController.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.sf.cloud.consul.discovery.client.ServiceAClient;
+import org.sf.cloud.consul.discovery.remote.PingClient;
 
 @RestController
 public class FeignClientController {
 
 	@Autowired
-	private ServiceAClient serviceAClient;
+	private PingClient pingClient;
 
 	@RequestMapping(value = "/feignClient")
 	public String feignClient() {
-		return serviceAClient.ping();
+		return pingClient.ping();
 	}
 
 }
